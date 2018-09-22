d3dfddba4cb2fda2265c624a80d41ffee714e55a
runnie <runnie@runnies-MacBook-Air.local>
2018-09-21 22:07:13 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String input = "Brockhampton is the best boy band since One Direction.";
+	System.out.println("input = "+input);
+	System.out.println("oddCharacters = "+oddCharacters(input));
+	System.out.println("evenCharacters = "+evenCharacters(input));
+	System.out.println("evenFirstOddSecond = "+evenFirstOddLast(input));
+    }
+    public static String evenFirstOddLast(String input){
+        return evenCharacters(input) + oddCharacters(input);
+    }
+    public static String evenCharacters(String input){
+        int index = 2;
+        String output = "";
+        while (index < input.length()){
+            output = output + input.substring(index,index+1);
+            index = index + 2;
+        }
+        return output;
+    }
+    public static String oddCharacters(String input){
+        int index = 1;
+        String output = "";
+        while (index < input.length())
+        {
+            output = output + input.substring(index,index+1);
+            index = index + 2;
+        }
+        return output;
+    }
+
+
+}
